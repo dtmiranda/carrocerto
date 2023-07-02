640cb5a6bb7f675e174dfd9ef922ec14c9e620bb
Samuel Miranda <hortadito2@gmail.com>
2023-07-02 12:00:55 -0100

    create NavBar and Footer component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; 
 
 import Image from "next/image";
 import CustomButton from "./CustomButton";
@@ -23,8 +23,17 @@ const Hero = () => {
         <CustomButton
           title = "Explore Cars"
           containerStyles = "bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll }
+          handleClick={ handleScroll }
         />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+
+          <div className="hero__image-overlay"></div>
+        </div>
+
       </div>
     </div>
   )
