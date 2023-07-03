a6e97c171c576f0f96dfed51ca3eb37b02d8b864
Samuel Miranda <hortadito2@gmail.com>
2023-07-03 22:34:23 -0100

    create search bar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import Image from 'next/image'
 
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home_text-container'>
+          <h1 className='text-4xl font-extrabold'>Catalogo</h1>
+          <p>Explore o carro que você pode gostar</p>
+
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title="fuel"/>
+            <CustomFilter title="year"/>
+
+          </div>
+        </div>
+      </div>
 
     </main>
   )
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,11 +13,11 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, or rent a car - quickly and easily!
+          Alugue um carro - de forma rápida e fácil!
         </h1>
         
         <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking process.
+          Simplifique sua experiência de aluguel de carros com nosso processo de reserva.
         </p>
 
         <CustomButton
